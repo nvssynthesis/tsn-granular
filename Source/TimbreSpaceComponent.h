68fc8afba827aeee798205b3377c47b18f671554
Nicholas Solem <nsolem@ucsd.edu>
2023-10-29 15:43:15 -0700

    further timbre space 2D plane development. surface now finds nearest point to what was clicked.

--- Source/TimbreSpaceComponent.h
+++ Source/TimbreSpaceComponent.h
@@ -11,6 +11,15 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "nvs_libraries/include/nvs_memoryless.h"
+#include "fmt/core.h"
+
+static bool inRange0_1(float x){
+	return ( (x >= 0.f) && (x <= 1.f) );
+}
+static bool inRangeM1_1(float x){
+	return ( (x >= -1.f) && (x <= 1.f) );
+}
 
 class TimbreSpaceComponent	:	public juce::Component
 {
@@ -31,23 +40,49 @@ class TimbreSpaceComponent	:	public juce::Component
 		// to easily trade hsv for rbg
 		std::array<juce::uint8, 3> toUnsigned() const {
 			for (auto p : p3D){
-				assert(p >= 0.f);
-				assert(p <= 1.f);
+				assert(inRangeM1_1(p));
 			}
+			using namespace nvs::memoryless;
 			std::array<juce::uint8, 3> u {
-				static_cast<juce::uint8>(p3D[0] * 255.f),
-				static_cast<juce::uint8>(p3D[1] * 255.f),
-				static_cast<juce::uint8>(p3D[2] * 255.f)
+				static_cast<juce::uint8>(biuni(p3D[0]) * 255.f),
+				static_cast<juce::uint8>(biuni(p3D[1]) * 255.f),
+				static_cast<juce::uint8>(biuni(p3D[2]) * 255.f)
 			};
 			return u;
 		}
 	};
+	juce::Array<timbre5DPoint> timbres5D;
+
 public:
 	TimbreSpaceComponent(){
 		timbres5D.add({.p2D{juce::Point(0.f, 0.f)}, .p3D{1.f, 0.5f, 0.2f}});
 		timbres5D.add({.p2D{0.5f, 0.5f}, .p3D{0.f, 0.8f, 0.9f}});
 	}
-	
+	void add2DPoint(float x, float y){
+		point_t p(x, y);
+		add2DPoint(p);
+	}
+	void add2DPoint(point_t p){
+		assert(inRangeM1_1(p.getX()) && inRangeM1_1(p.getY()));
+		timbres5D.add({
+			.p2D{p},
+			.p3D{1.f, 1.f, 1.f}
+		});
+	}
+	void add5DPoint(point_t p2D, std::array<float, 3> p3D){
+		assert(inRangeM1_1(p2D.getX()) && inRangeM1_1(p2D.getY())
+			   && inRangeM1_1(p3D[0]) && inRangeM1_1(p3D[1]) && inRangeM1_1(p3D[2])
+			   );
+		timbres5D.add({
+			.p2D{p2D},
+			.p3D{p3D}
+		});
+	}
+	void add5DPoint(float x, float y, float z, float w, float v){
+		point_t p2D(x,y);
+		std::array<float, 3> p3D{z, w, v};
+		add5DPoint(p2D, p3D);
+	}
 	void paint(juce::Graphics &g) override {
 		g.setOpacity(0.75f);
 		g.fillAll(juce::Colours::purple);
@@ -59,9 +94,9 @@ public:
 		
 		auto pointToRect = [](point_t p) -> juce::Rectangle<float> {
 			point_t upperLeft{p}, bottomRight{p};
-			constexpr float dotSize {3.f};
-			upperLeft.addXY(-dotSize, -dotSize);
-			bottomRight.addXY(dotSize, dotSize);
+			constexpr float halfDotSize {2.f};
+			upperLeft.addXY(-halfDotSize, -halfDotSize);
+			bottomRight.addXY(halfDotSize, halfDotSize);
 			return juce::Rectangle<float>(upperLeft, bottomRight);
 		};
 		auto transformFromZeroOrigin = [](point_t p) -> point_t {
@@ -69,13 +104,6 @@ public:
 //			output bounds: ([0..1], [0..1])
 			p.addXY(1.f, 1.f);	// [0..2]
 			p *= 0.5f;			// [-1..1]
-			return p;
-		};
-		auto transformToZeroOrigin = [](point_t p) -> point_t {
-//			input bounds:  ([0..1], [0..1])
-//			output bounds: ([-1..1], [-1..1])
-			p *= 2.f;			// [0..2]
-			p.addXY(-1.f, -1.f);// [-1..1]
 			return p;
 		};
 		for (auto p5 : timbres5D){
@@ -88,19 +116,39 @@ public:
 			g.fillEllipse(rect);
 		}
 	}
-	
-	void resized() override {
-		
-	}
-	void mouseMove (const juce::MouseEvent &event) override {
-		
+	void resized() override {}
+
+	int findNearestPoint(float x, float y){
+		float diff = 1e15;
+		int idx = 0;
+		for (int i = 0; i < timbres5D.size(); ++i){
+			auto p = timbres5D[i].get2D();
+			auto xx = (p.getX() - x);
+			auto yy = (p.getY() - y);
+			auto tmp = xx*xx + yy*yy;
+			if (tmp < diff){
+				diff = tmp;
+				idx = i;
+			}
+		}
+		return idx;
 	}
 	void mouseDown (const juce::MouseEvent &event) override {
-		
+		[[maybe_unused]] float x = static_cast<float>(event.getMouseDownX());
+		[[maybe_unused]] float y = static_cast<float>(event.getMouseDownY());
+		auto bounds = getLocalBounds().toFloat();
+		x /= bounds.getWidth();
+		y /= bounds.getHeight();
+		using namespace nvs::memoryless;
+		x = unibi(x);
+		y = unibi(y);
+		fmt::print("in mouseDown at {}, {}\n", x, y);
+		int nearestIdx = findNearestPoint(x, y);
+		auto nearestPoint = timbres5D[nearestIdx].get2D();
+		fmt::print("nearestIdx: {}, point x,y: {},{}\n",
+				   nearestIdx, nearestPoint.getX(), nearestPoint.getY());
 	}
-	
 private:
-	juce::Array<timbre5DPoint> timbres5D;
 	
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TimbreSpaceComponent);
 };
