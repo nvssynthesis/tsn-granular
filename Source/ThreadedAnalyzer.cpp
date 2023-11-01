189bd1deb5b706883773f2100b2d3914377f999a
Nicholas Solem <nsolem@ucsd.edu>
2023-11-01 16:17:48 -0700

    threadedAnalyzer begun

---
+++ Source/ThreadedAnalyzer.cpp
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    ThreadedAnalyzer.cpp
+    Created: 1 Nov 2023 3:36:32pm
+    Author:  Nicholas Solem
+
+  ==============================================================================
+*/
+
+#include "ThreadedAnalyzer.h"
+#include "fmt/core.h"
+
+namespace nvs {
+namespace analysis {
+
+ThreadedAnalyzer::ThreadedAnalyzer()
+:	juce::Thread("Analyzer")
+{}
+
+void ThreadedAnalyzer::run() {
+	switch (_analysisType) {
+		case (analysisType_e::onset):
+			fmt::print("onset analysis\n");
+			calculateOnsets(<#std::vector<float> wave#>)
+			break;
+		case (analysisType_e::bfcc):
+			fmt::print("bfcc analysis\n");
+			break;
+		case (analysisType_e::pca):
+			fmt::print("pca analysis\n");
+			break;
+	}
+}
+
+}	// namespace analysis
+}	// namespace nvs
---
+++ Source/ThreadedAnalyzer.h
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    ThreadedAnalyzer.h
+    Created: 1 Nov 2023 3:36:32pm
+    Author:  Nicholas Solem
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "Analyzer.h"
+#include <JuceHeader.h>
+
+namespace nvs {
+namespace analysis {
+
+class ThreadedAnalyzer	:	public Analyzer
+,							public juce::Thread
+{
+public:
+	ThreadedAnalyzer();
+	void run() override;
+	
+	enum analysisType_e {
+		onset,
+		bfcc,
+		pca
+	};
+	void setAnalysisType(analysisType_e a) {
+		_analysisType = a;
+	}
+	
+private:
+	analysisType_e _analysisType { analysisType_e::onset };
+	
+};
+
+}	// namespace analysis
+}	// namespace nvs
