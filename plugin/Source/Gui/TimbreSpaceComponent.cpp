a0dca04d7bf563bb6b3a7d99ec738beb5f6ddfb1
Nicholas Solem <nsolem@ucsd.edu>
2025-10-23 00:01:10 +0200

    remove shadowed variable

--- plugin/Source/Gui/TimbreSpaceComponent.cpp
+++ plugin/Source/Gui/TimbreSpaceComponent.cpp
@@ -255,18 +255,18 @@ void TimbreSpaceComponent::paint(juce::Graphics &g) {
 
                         // Calculate both points on Catmull-Rom curve
                         auto calcPoint = [&](float t) -> juce::Point<float> {
-                            float t2 = t * t;
-                            float t3 = t2 * t;
+                            float t_sq = t * t;
+                            float t_cube = t_sq * t;
 
                             float x = 0.5f * ((2.0f * p1.x) +
                                               (-p0.x + p2.x) * t +
-                                              (2.0f * p0.x - 5.0f * p1.x + 4.0f * p2.x - p3.x) * t2 +
-                                              (-p0.x + 3.0f * p1.x - 3.0f * p2.x + p3.x) * t3);
+                                              (2.0f * p0.x - 5.0f * p1.x + 4.0f * p2.x - p3.x) * t_sq +
+                                              (-p0.x + 3.0f * p1.x - 3.0f * p2.x + p3.x) * t_cube);
 
                             float y = 0.5f * ((2.0f * p1.y) +
                                               (-p0.y + p2.y) * t +
-                                              (2.0f * p0.y - 5.0f * p1.y + 4.0f * p2.y - p3.y) * t2 +
-                                              (-p0.y + 3.0f * p1.y - 3.0f * p2.y + p3.y) * t3);
+                                              (2.0f * p0.y - 5.0f * p1.y + 4.0f * p2.y - p3.y) * t_sq +
+                                              (-p0.y + 3.0f * p1.y - 3.0f * p2.y + p3.y) * t_cube);
 
                             return {x, y};
                         };
